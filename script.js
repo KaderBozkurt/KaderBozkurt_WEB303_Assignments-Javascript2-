8b24954a8d0f303576633f2bb934379a095a4734
KaderBozkurt <98470403+KaderBozkurt@users.noreply.github.com>
2022-09-21 11:50:13 -0400

    third

--- script.js
+++ script.js
@@ -8,12 +8,26 @@
 
 		$(document).ready(function(){
 			$("#amount").change()
-			var salary=Number($("#yearly-salary").val());
-			var percent=Number($("#percent").val());
-			var result=percent*salary/100;
-			let n = result.toFixed(2);	
-			document.getElementById("amount").innerHTML=" $"+n;
+			var $salary=Number($("#yearly-salary").val());
+			var $percent=Number($("#percent").val());
+			var $result= ($percent*$salary)/100;
+			let $n = $result.toFixed(2);	
+			//document.getElementById("amount").innerHTML=" $"+n;
+			$("#amount").html('$' + $n);
 			
+			
+			/*$("input").keyup(function(){ 
+				 
+				$("#yearly-salary").css("background-color", "yellow"); 
+				$("#percent").css("background-color", "lightgreen");  
+
+				var $salary=Number($("#yearly-salary").val());
+				var $percent=Number($("#percent").val());
+				var $result = $percent * $salary/100;
+				let $n = $result.toFixed(2);	
+				$("#amount").html('$' + $n);
+				
+			});  */
 			});
 		
 	
