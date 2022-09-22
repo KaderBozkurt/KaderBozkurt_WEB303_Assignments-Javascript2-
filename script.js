424a7b44ab81297788c02012cd3d0fd8043e7ed8
KaderBozkurt <98470403+KaderBozkurt@users.noreply.github.com>
2022-09-21 20:49:20 -0400

    four

--- script.js
+++ script.js
@@ -7,27 +7,24 @@
 
 
 		$(document).ready(function(){
-			$("#amount").change()
-			var $salary=Number($("#yearly-salary").val());
-			var $percent=Number($("#percent").val());
-			var $result= ($percent*$salary)/100;
+			/*$("input").change(function(){
+			let $salary=Number($("#yearly-salary").val());
+			let $percent=Number($("#percent").val());
+			let $result= ($percent*$salary)/100;
 			let $n = $result.toFixed(2);	
-			//document.getElementById("amount").innerHTML=" $"+n;
 			$("#amount").html('$' + $n);
+			});*/
 			
 			
-			/*$("input").keyup(function(){ 
+			$("input").keyup(function(){
 				 
-				$("#yearly-salary").css("background-color", "yellow"); 
-				$("#percent").css("background-color", "lightgreen");  
-
-				var $salary=Number($("#yearly-salary").val());
-				var $percent=Number($("#percent").val());
-				var $result = $percent * $salary/100;
+				let $salary=Number($("#yearly-salary").val());
+				let $percent=Number($("#percent").val());
+				let $result = $percent * $salary/100;
 				let $n = $result.toFixed(2);	
 				$("#amount").html('$' + $n);
-				
-			});  */
+			});	
+			
 			});
 		
 	
