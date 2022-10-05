656982f97d7c4b5a7a1eaf1e91c54d18775c5385
KaderBozkurt <98470403+KaderBozkurt@users.noreply.github.com>
2022-10-05 18:40:40 -0400

    second

--- js/main.js
+++ js/main.js
@@ -22,17 +22,60 @@
         $("#content").load("retain.html");
       });
 
+/*
 
-     $(document).ready(function(){
-        $("#content").click(function(){
-          var div = $("div");
-          div.animate({
-          height: '+=750px',
-          width: '+=300px'});
-    
-        
-        });
-      }); 
-      
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("content").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "prospect.html");
+  xhttp.send();
+};
+
+
+function loadDoc1() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("content").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "convert.html");
+  xhttp.send();
+};
+
+
+function loadDoc2() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("content").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "retain.html");
+  xhttp.send();
+};
+*/
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#content").click(function(){
+    var div = $("div");
+    div.animate({
+      width: "90%",
+      opacity: 0.7,
+      marginRight: "0.6in",
+      fontSize: "1.5em",
+      borderWidth: "2px" 
+    }, 2500 );
+
+  
+  });
+}); 
       
 
